697d13c2829522262f2c8457eca79292caa9f743
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-17 13:13:14 +0200

    Update Image-downloader.py
    Adjusted comments

--- src/Image-downloader.py
+++ src/Image-downloader.py
@@ -1,12 +1,14 @@
-from pandas import *
-import requests
+from pandas import * # pip install pandas
+import requests # pip install requests
 
+# -------------------- Setup --------------------
 # Reading CSV file
 data = read_csv("CSV Files/Decathlon-product-reviews.csv")
 
 # converting column data to list
 Images = data['Img'].tolist()
 
+# -------------------- Methods --------------------
 # Method to download all images from the links in the list
 def imageDownloader():
     for imageURL in Images:
@@ -15,4 +17,5 @@ def imageDownloader():
         with open('Images/' + filename, 'wb') as handler:
             handler.write(response)
 
+# -------------------- Run --------------------
 imageDownloader()
