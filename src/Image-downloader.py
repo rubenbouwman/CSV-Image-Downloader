a3f5bac5257b94dba279164d48a91c24da6b7cd1
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-17 13:08:20 +0200

    Finished the downloader
    Downloader is fully functional now

--- src/Image-downloader.py
+++ src/Image-downloader.py
@@ -1,4 +1,5 @@
 from pandas import *
+import requests
 
 # Reading CSV file
 data = read_csv("CSV Files/Decathlon-product-reviews.csv")
@@ -6,4 +7,12 @@ data = read_csv("CSV Files/Decathlon-product-reviews.csv")
 # converting column data to list
 Images = data['Img'].tolist()
 
-print(Images[3])
+# Method to download all images from the links in the list
+def imageDownloader():
+    for imageURL in Images:
+        response = requests.get(imageURL).content
+        filename = imageURL.split('/')[-1]
+        with open('Images/' + filename, 'wb') as handler:
+            handler.write(response)
+
+imageDownloader()
