963972fd5dfa36bc16cbe14e12000cb794f8dc7a
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-17 12:20:34 +0200

    Update Image-downloader.py
    Fixed csv file path, image links and reviews are put into their own lists now.

--- src/Image-downloader.py
+++ src/Image-downloader.py
@@ -1,4 +1,10 @@
 from pandas import *
 
 # Reading CSV file
-data = read_csv("Decathlon-product-reviews.csv")
+data = read_csv("CSV Files/Decathlon-product-reviews.csv")
+
+# converting column data to list
+Images = data['Img'].tolist()
+Review = data['Review'].tolist()
+
+print(Images)
